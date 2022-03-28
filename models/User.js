a25a9bdc5ea937e37e55c63993300ae639201a73
unknown <steinway.nathan@gmail.com>
2022-03-27 22:55:59 -0500

    updated readme

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const UserSchema = new Schema(
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
-                ref: 'thought'
+                ref: 'Thought'
             }
         ],
 
